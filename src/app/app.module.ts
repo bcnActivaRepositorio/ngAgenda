9522362924ffea67ef25f3ca174cf7cbec3bb182
naranjito72 <theclick36@gmail.com>
2021-02-11 12:40:08 +0100

    validationsOne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +28,7 @@ import { LandingComponent } from './components/landing/landing.component';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pr-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
+
+  title: string = "Mail Me";
+
+  userMails = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+     // Validators.pattern("[\p{L}]"),
+      Validators.minLength(3)
+    ]),
+    username: new FormControl('', [
+      Validators.required,
+     // Validators.pattern("[\p{L}0-9]"),
+      Validators.minLength(3)
+    ]),
+    myMail: new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+      Validators.email
+    ])
+  });
   constructor() { }
+// getter
+get controlMe() {
+
+  return this.userMails.controls;
+}
 
   ngOnInit(): void {
   }
 
+  // see the data frm the form
+submitMe(): void{
+  console.log(this.userMails.value);
+}
 }
