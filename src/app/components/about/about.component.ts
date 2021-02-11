21bac695ac861698ddcbe02bcd7fb0123fddbbfc
naranjito72 <theclick36@gmail.com>
2021-02-11 13:59:41 +0100

    validation2

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'pr-about',
@@ -12,24 +12,28 @@ export class AboutComponent implements OnInit {
 
   title: string = "Mail Me";
 
-  userMails = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-     // Validators.pattern("[\p{L}]"),
-      Validators.minLength(3)
-    ]),
-    username: new FormControl('', [
-      Validators.required,
-     // Validators.pattern("[\p{L}0-9]"),
-      Validators.minLength(3)
-    ]),
-    myMail: new FormControl('', [
-      Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
-      Validators.email
-    ])
-  });
-  constructor() { }
+
+constructor() {
+
+ }
+ userMails = new FormGroup({
+  name: new FormControl('', [
+    Validators.required,
+   // Validators.pattern("[\p{L}]"),
+    Validators.minLength(3)
+  ]),
+  username: new FormControl('', [
+    Validators.required,
+   // Validators.pattern("[\p{L}0-9]"),
+    Validators.minLength(3)
+  ]),
+  myMail: new FormControl('', [
+    Validators.required,
+    Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+    Validators.email
+  ])
+});
+
 // getter
 get controlMe() {
 
@@ -38,7 +42,6 @@ get controlMe() {
 
   ngOnInit(): void {
   }
-
   // see the data frm the form
 submitMe(): void{
   console.log(this.userMails.value);
